e39359adec7bfd110818eada8186c6907b62b856
Anton Rufino <anton.vongola@gmail.com>
2016-08-29 05:38:18 +0800

    Add student routes.

--- back-end/config/router.js
+++ back-end/config/router.js
@@ -4,8 +4,13 @@ const importer = require('anytv-node-importer');
 module.exports = (router) => {
     const __ = importer.dirloadSync(__dirname + '/../controller');
 
-    router.get('/', __.hello.hello);
-    router.post('/hello/name', __.hello.helloName);
+    router.put('/add', __.student.add);
+    router.get('/view/all', __.student.viewAll);
+    router.get('/view/student/:batch/:number', __.student.view);
+    router.get('/view/filter/:filter', __.student.viewWithFilter);
+    router.get('/search', __.student.search);
+    router.get('/edit', __.student.edit);
+    router.delete('/delete', __.student.delete);
 
     return router;
 }
