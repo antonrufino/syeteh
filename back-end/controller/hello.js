87dd7d07ef56d5a4e8f71a8455fb50546d8845b6
Anton Rufino <anton.vongola@gmail.com>
2016-08-29 05:38:37 +0800

    Remove test route

--- back-end/controller/hello.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.hello = (req, res, next) => {
-    res.send('Hello world!');
-}
-
-exports.helloName = (req, res, next) => {
-    res.send('Hello ' + req.body.name + '!');
-}
